14d0ab32f0d355f0a04d8c8933e4d76f1f9cce24
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-04 17:52:44 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -10,3 +10,29 @@ let person = {
 };
 
 person.greet(); // my name is Bob, hi!
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
+// Uncaught TypeError: Cannot read property 'interests' of undefined
