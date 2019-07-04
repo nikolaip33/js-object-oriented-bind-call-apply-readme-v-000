fe8b2470d26de8da4f11ae8b0ff8cf34a1aa56a3
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-04 17:53:14 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+greet(); // my name is , hi!
+
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+
+person.greet(); // my name is Bob, hi!
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
+// Uncaught TypeError: Cannot read property 'interests' of undefined
